b35f18d5baf9a1446ee830509a502f2f61f24571
juyeonkim97 <lhkl@naver.com>
2020-11-10 00:56:07 +0900

    파일명을 바로 가져와서 출력하도록 바꿈

--- ppt_automation.py
+++ ppt_automation.py
@@ -14,17 +14,16 @@ pleft=Inches(0.7)
 ptop=Inches(1.5)
 pwidth=Inches(10)
 pheight=Inches(7)
-for i in range(1,20):
-    img_path='images\\'+str(ch)+'-'+str(i)+'.png'
-    if os.path.exists(img_path):
-        blank_slide_layout = prs.slide_layouts[6]
-        slide = prs.slides.add_slide(blank_slide_layout)
-        txBox = slide.shapes.add_textbox(left, top,width,height)
-        tf = txBox.text_frame
-        p = tf.add_paragraph()
-        p.text = '예제 '+str(ch)+'- '+str(i)
-        p.font.size = Pt(33)
-        pic = slide.shapes.add_picture(img_path,pleft,ptop,pwidth,pheight)
+for i in os.listdir('images'):
+    full_path = os.path.join('images\\', i)
+    blank_slide_layout = prs.slide_layouts[6]
+    slide = prs.slides.add_slide(blank_slide_layout)
+    txBox = slide.shapes.add_textbox(left, top,width,height)
+    tf = txBox.text_frame
+    p = tf.add_paragraph()
+    p.text = '예제 '+ os.path.splitext(i)[0]
+    p.font.size = Pt(33)
+    pic = slide.shapes.add_picture(full_path,pleft,ptop,pwidth,pheight)
 
 filename=str(week)+'주차 김주연.pptx'
 prs.save(filename)
