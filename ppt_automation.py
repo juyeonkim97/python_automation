046733624bdd09039349e02ab817418fdd7484d5
juyeonkim97 <lhkl@naver.com>
2020-11-11 00:05:08 +0900

    템플릿 적용, 슬라이드 번호 추가

--- ppt_automation.py
+++ ppt_automation.py
@@ -2,28 +2,22 @@ from pptx import Presentation
 from pptx.util import Inches,Pt
 import os
 
-prs = Presentation()
-prs.slide_width = Inches(16)
-prs.slide_height = Inches(9)
-ch=input('input your chapter :')
-week=input('input week: ')
-left = Inches(0.7)
-top = Inches(0.2)
-width = height = Inches(1)
-pleft=Inches(0.7)
-ptop=Inches(1.5)
-pwidth=Inches(10)
-pheight=Inches(7)
+prs = Presentation("template.pptx")
+week=input('몇 주차 과제임? : ')
+pleft=Inches(1)
+ptop=Inches(1.6)
+pwidth=Inches(7.5)
+pheight=Inches(5.5)
+j=1
 for i in os.listdir('images'):
     full_path = os.path.join('images\\', i)
-    blank_slide_layout = prs.slide_layouts[6]
-    slide = prs.slides.add_slide(blank_slide_layout)
-    txBox = slide.shapes.add_textbox(left, top,width,height)
-    tf = txBox.text_frame
-    p = tf.add_paragraph()
-    p.text = '예제 '+ os.path.splitext(i)[0]
-    p.font.size = Pt(33)
+    slide = prs.slides.add_slide(prs.slide_layouts[5])
+    title=slide.shapes.title
+    title.text='예제 '+ os.path.splitext(i)[0]
+    slide_num=slide.placeholders[1]
+    slide_num.text=str(j)
     pic = slide.shapes.add_picture(full_path,pleft,ptop,pwidth,pheight)
+    j+=1
 
 filename=str(week)+'주차 김주연.pptx'
 prs.save(filename)
